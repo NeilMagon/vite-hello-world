9b5f888b2392165e1cbe4815785605ded67d4f37
NeilMagon <neil.magon97@gmail.com>
2024-04-10 17:24:12 +0200

    feat: add AppHero components and add img

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppHero',
+}
+</script>
+
+<template>
+    <div>
+        <img src="../assets/img/vite.png" alt="">
+    </div>
+</template>
+
+<style>
+    img{
+        border-radius: 1.25rem;
+    }
+    div{
+        text-align: center;
+    }
+</style>
